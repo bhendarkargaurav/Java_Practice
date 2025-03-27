07bf8fc7486cdd99abe3ccf88897424da95b6b8d
Gaurav Bhendarkar <157362777+bhendarkargaurav@users.noreply.github.com>
2025-03-27 08:13:12 +0530

    Update array1.java

--- array1.java
+++ array1.java
@@ -1,26 +1,4 @@
-// import java.util.*;
 
-// import javax.print.attribute.standard.OutputDeviceAssigned;
-
-// public class array1 {
-//     public static void main(String args[]) {
-//         Scanner sc = new Scanner(System.in);
-
-//         // size of array
-//         int size = sc.nextInt();
-//         int numbers[] = new int[size];
-
-//         // input value for array      
-//         for(int i=0; i<size; i++){    
-//             numbers[i] = sc.nextInt();
-//         }
-
-//         // for Output
-//         for(int i=0; i<size; i++){
-//             System.out.print(numbers[i]);
-//         }     
-//  }
-// }
 
 import java.util.*;
 public class array1{
